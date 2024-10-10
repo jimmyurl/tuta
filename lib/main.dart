49d42dd92839cdfc923e977dd77ba5bd9f461827
jimmyurl <jimmy.james365@gmail.com>
2024-10-10 14:15:19 +0300

    supabase functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,7 @@ import 'package:tutor/ui/profile_screen.dart'; // Import the ProfileScreen
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is initialized
   await Supabase.initialize(
-    url:
-        'https://xfihpvkbzppaejluyqoq.supabase.co', // Replace with your Supabase URL
+    url: 'supabase url', // Replace with your Supabase URL
     anonKey: 'your_anon_key', // Replace with your Supabase anon key
   );
   runApp(MeetYourTutorApp());
